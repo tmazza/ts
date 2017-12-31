a0f2ad48b7ff59437201ea2dfe1f4101dda6e81b
tmazza <tiagomdepaula@gmail.com>
2017-12-31 16:08:43 -0200

    Search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,11 @@ import { IonicPage, NavController } from 'ionic-angular';
 import { AppConfig } from '../../app-config';
 import { ApiProvider } from '../../providers/api-provider';
 
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/mergeMap';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -10,16 +15,29 @@ import { ApiProvider } from '../../providers/api-provider';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public api: ApiProvider) {
+  public searchField: FormControl;
+  public searchForm: FormGroup;
+  public searchResults:any = [];
 
-    let query = "vikings";
-    this.api.searchTVShows(query).subscribe(
-      (res) => { console.log('res', res); },
-      (err) => { console.log('err', err); },
-      () => { },
-    );
+  constructor(public navCtrl: NavController, public api: ApiProvider, private fb:FormBuilder) {
+    this.searchField = new FormControl();
+    this.searchForm = this.fb.group({search: this.searchField});
 
-    console.log(AppConfig.API_ENDPOINT);
+    this.searchField.valueChanges
+          .debounceTime(400)
+          .flatMap(term => this.api.searchTVShows(term) )
+          .subscribe(res => {
+            this.searchResults = res.results;
+            console.log(this.searchResults);
+          })
+  }
+
+  getPosterPath(result) {
+    return result.poster_path ? AppConfig.URL_IMAGE  + '/w185/' + result.poster_path : AppConfig.DEFAULT_POSTER;
+  }
+
+  getBackdropPath(result) {
+    return result.backdrop_path ? AppConfig.URL_IMAGE  + '/w342/' + result.backdrop_path : AppConfig.DEFAULT_BACKDROP;
   }
 
 }
