8efa9425108b751c2442e44cf5b4d52c4ad7baa6
tmazza <tiagomdepaula@gmail.com>
2017-12-31 14:15:36 -0200

    Root

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = "HomePage";
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, 
+              public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    this.pages = [
+      { title: 'Home', component: "HomePage" },
+      { title: 'List', component: "ListPage" }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+}
